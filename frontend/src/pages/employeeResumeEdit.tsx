44003e05cd278d4dae0cc59d5ea5a8b4d439ab29
jasonDeichert <jasondeichert@gmail.com>
2024-07-31 17:50:51 -0400

    ugly first pass at resume edit page

--- frontend/src/pages/employeeResumeEdit.tsx
+++ frontend/src/pages/employeeResumeEdit.tsx
@@ -1,1 +1,14 @@
 //page for candidates to review and edit resume. 
+import React from 'react';
+import PdfUploadForm from '../components/employeeResumeEdit/pdfUploadForm';
+
+const EmployeeResumeEdit: React.FC = () => {
+    return (
+        <div>
+            <h1>Resume Edit Page</h1>
+            <PdfUploadForm />
+        </div>
+    );
+};
+
+export default EmployeeResumeEdit;
