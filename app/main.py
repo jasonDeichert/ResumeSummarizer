ed49f0b374ec1fd2f9f72ade321cbecdbe7f93c6
jasonDeichert <jasondeichert@gmail.com>
2024-08-04 15:12:08 -0400

    updated main to run api instead of initial test

--- app/main.py
+++ app/main.py
@@ -1,21 +1,8 @@
-# api will come later
-# from fastapi import FastAPI
-# from app.router import resume
+from fastapi import FastAPI, Depends
+from app.router import resume
+from app.services.client import Client
 
-# app = FastAPI()
+client = Client()
 
-# app.include_router(resume.router, tags=["summarizeRecipe"])
-
-from utils.pdf import Parse_PDF
-from services.resume.standardize import standardize_resume
-import asyncio
-
-async def main():
-    pdf_file_location = input("Enter the path to the pdf file: ")
-    parsed_pdf = Parse_PDF(pdf_file_location)
-    resume = await standardize_resume(parsed_pdf.text_content_and_styles)
-    print(resume)
-
-
-asyncio.run(main())
-
+app = FastAPI()
+app.include_router(resume.router, tags=["summarizeRecipe"], dependencies=[Depends(client.get_client)])
