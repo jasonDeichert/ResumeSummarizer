16abfd667139f7beab2549e0dde8835c06cc045b
jasonDeichert <jasondeichert@gmail.com>
2024-08-04 15:17:37 -0400

    added type checking. Fitz doesn't have comprehensive type hinting though, so I am ignoring some type imports here so I can leave strict typing on. Might be worth re-considering library choice.

--- app/utils/pdf.py
+++ app/utils/pdf.py
@@ -1,32 +1,34 @@
-import fitz #this seems like it takes a long time to load the first time it's imported. We should try to make sure this is done when api service starts to avoid a slow first user xp.
-from pydantic import BaseModel
-from model.db.resume import ResumeEducation, ResumeExperience, Resume
-from model.db.pdf import Text_Section_and_Style
-
-#we need to add some server side validation of pdfs before we parse them
-#we should also add some error handling for when the pdf is not in the correct format
+import fitz # type: ignore
+#this seems like it takes a long time to load the first time it's imported. We should try to make sure this is done when api service starts to avoid a slow first user xp.
+from app.model.db.pdf import Text_Section_and_Style
+from typing import List, BinaryIO
 
 class Parse_PDF:
-    def __init__(self, pdf_file: str):
-        self.pdf_file: str = pdf_file
+    def __init__(self, pdf_file: BinaryIO) -> None:
+        self.pdf_file: BinaryIO = pdf_file
         self.pdf_object: fitz.Document = self.deserialize_pdf(pdf_file)
-        self.text_content_and_styles: list[dict] = self.extract_text_content_and_styles(self.pdf_object)
+        self.text_content_and_styles: List[Text_Section_and_Style] = self.extract_text_content_and_styles(self.pdf_object)
 
-    #we should add some validation on document size
-    def deserialize_pdf(self, pdf_file: str) -> fitz.Document:
-        pdf_object = fitz.open(pdf_file)
+    def deserialize_pdf(self, pdf_file: BinaryIO) -> fitz.Document:
+        pdf_object: fitz.Document = fitz.open(stream=pdf_file.read(), filetype="pdf")
         return pdf_object
-    def extract_text_content_and_styles(self, pdf_object: fitz.Document) -> list[Text_Section_and_Style]:
-        text_content_and_styles = []
+
+    def extract_text_content_and_styles(self, pdf_object: fitz.Document) -> List[Text_Section_and_Style]:
+        text_content_and_styles: List[Text_Section_and_Style] = []
                 
         for page_num in range(len(pdf_object)):
-            page = pdf_object.load_page(page_num)
-            blocks = page.get_text("dict")["blocks"]
+            page = pdf_object.load_page(page_num)  # type: ignore
+            blocks = page.get_text("dict")["blocks"]  # type: ignore
 
-            for block in blocks:
+            for block in blocks:  # type: ignore
                 if "lines" in block:
-                    for line in block["lines"]:
-                        for span in line["spans"]:
-                            text_content_and_styles.append(Text_Section_and_Style(text = span["text"], size = span["size"], font = span["font"], color = str(span["color"]))) 
+                    for line in block["lines"]:  # type: ignore
+                        for span in line["spans"]:  # type: ignore
+                            text_content_and_styles.append(Text_Section_and_Style(
+                                text=span["text"], # type: ignore
+                                size=span["size"], # type: ignore
+                                font=span["font"], # type: ignore
+                                color=str(span["color"]) # type: ignore
+                            ))  # type: ignore
 
         return text_content_and_styles
