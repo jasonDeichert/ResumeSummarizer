8f6178924f02e1974c367cc1ff52d588860f78b9
jasonDeichert <jasondeichert@gmail.com>
2024-08-04 15:15:06 -0400

    updated router to use file instead of an object removed unnecessary imports

--- app/router/resume.py
+++ app/router/resume.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, HTTPException, Depends
-import app.controller.resume as resume_controller
+# app/router/resume.py
+from fastapi import APIRouter, UploadFile, File, Depends
+from app.controller import resume as resume_controller
 from app.model.api.out import StandardizeResumeOut
+from app.services.client import Client
 
 router = APIRouter()
 
 @router.post("/standardizeresume", response_model=StandardizeResumeOut)
-async def standardize_resume(resume: str = Depends(resume_controller.standardize_resume)):
-    return resume
+async def standardize_resume(file: UploadFile = File(...), client: Client = Depends(Client.get_client)) -> StandardizeResumeOut:
+    return await resume_controller.standardize_resume(file, client)
