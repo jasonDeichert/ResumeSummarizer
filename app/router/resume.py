371b83397e44b799765775f8cc04b438245bb90c
jasonDeichert <jasondeichert@gmail.com>
2024-08-04 16:44:33 -0400

    more useful print statement

--- app/router/resume.py
+++ app/router/resume.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter, UploadFile, File, Depends
 from app.controller import resume as resume_controller
 from app.model.api.out import StandardizeResumeOut
 from app.services.client import Client
+from datetime import datetime
 
 router = APIRouter()
 
 @router.post("/standardizeresume", response_model=StandardizeResumeOut)
 async def standardize_resume(file: UploadFile = File(...), client: Client = Depends(Client.get_client)) -> StandardizeResumeOut:
+    print(f"Received request to standardize a resume: {datetime.now()}")
     return await resume_controller.standardize_resume(file, client)
