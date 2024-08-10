3d095cfcc14ee620ce13b4b16ed1b40b8988a525
jasonDeichert <jasondeichert@gmail.com>
2024-08-10 09:25:27 -0400

    summarize resume controller

--- app/controller/resume.py
+++ app/controller/resume.py
@@ -1,4 +1,5 @@
-import app.services.resume.standardize as s
+import app.services.resume.standardize as standardize_service
+import app.services.resume.summarize as summarize_service
 from fastapi import UploadFile
 import app.model.api.out as apimodelout
 import asyncio
@@ -11,6 +12,11 @@ async def standardize_resume(file: UploadFile, client: Client) -> apimodelout.St
     #convert file to BinaryIO
     file_binary: BinaryIO = BytesIO(await file.read())
     parsed_pdf: Parse_PDF = Parse_PDF(file_binary)
-    standardize_resume_task: Awaitable[apimodelout.StandardizeResumeOut] = asyncio.create_task(s.standardize_resume(parsed_pdf.text_content_and_styles, client))
+    standardize_resume_task: Awaitable[apimodelout.StandardizeResumeOut] = asyncio.create_task(standardize_service.standardize_resume(parsed_pdf.text_content_and_styles, client))
     standardize_resume: apimodelout.StandardizeResumeOut = await standardize_resume_task
     return standardize_resume
+
+async def summarize_resume(resume: apimodelout.StandardizeResumeOut, client: Client) -> apimodelout.SummarizeResumeOut:
+    summarize_resume_task: Awaitable[apimodelout.SummarizeResumeOut] = asyncio.create_task(summarize_service.summarize_resume(resume, client))
+    summarize_resume: apimodelout.SummarizeResumeOut = await summarize_resume_task
+    return summarize_resume
