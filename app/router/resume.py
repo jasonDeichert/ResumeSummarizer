4534a500cf3cf62d7a21770c1826344cf3e251bc
jasonDeichert <jasondeichert@gmail.com>
2024-08-10 09:29:39 -0400

    summarize route

--- app/router/resume.py
+++ app/router/resume.py
@@ -1,7 +1,7 @@
 # app/router/resume.py
 from fastapi import APIRouter, UploadFile, File, Depends
 from app.controller import resume as resume_controller
-from app.model.api.out import StandardizeResumeOut
+from app.model.api.out import StandardizeResumeOut, SummarizeResumeOut
 from app.services.client import Client
 from datetime import datetime
 
@@ -11,3 +11,8 @@ router = APIRouter()
 async def standardize_resume(file: UploadFile = File(...), client: Client = Depends(Client.get_client)) -> StandardizeResumeOut:
     print(f"Received request to standardize a resume: {datetime.now()}")
     return await resume_controller.standardize_resume(file, client)
+
+@router.post("/summarizeresume", response_model=SummarizeResumeOut)
+async def summarize_resumse(resume: StandardizeResumeOut, client: Client = Depends(Client.get_client)) -> SummarizeResumeOut:
+    print(f"Received request to summarize a resume: {datetime.now()}")
+    return await resume_controller.summarize_resume(resume, client)
