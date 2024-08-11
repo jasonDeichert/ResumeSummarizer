53e554a4542d6a7df121717f973f8d2edd2ab396
jasonDeichert <jasondeichert@gmail.com>
2024-08-10 20:54:59 -0400

    tweaks to summarize prompt

--- app/services/resume/summarize.py
+++ app/services/resume/summarize.py
@@ -5,13 +5,15 @@ from app.utils.describe_class import describe_class
 from app.services.client import Client
 
 async def summarize_resume(resume: StandardizeResumeOut, client: Client) -> SummarizeResumeOut:
-    system_content: str = f'''You summarize resumes into as few words as possible for an employer. Be skeptical and honest.
+    system_content: str = f'''You summarize resumes into as few words as possible for an employer. Be skeptical and honest (don't oversell the candidate).
+    The summary will be used to quickly compare potential candidates and act as a quick introduction for employers doing many interviews.
     You will receive an object with the following schema:
     {describe_class(StandardizeResumeOut)}
     Your job is to summarize this into a single paragraph that is succinct as possible, containing the most important information.
+    Give an idea of how much school or work experience they have in number of years.
     Return a JSON object with this schema:
     {describe_class(SummarizeResumeOut)}
-    Employability should be on a scale of 1-10 with a bell curve distribution centered at 5.'''
+    The average general employability rating should be 5 (out of 10), which would represent a first year university student that is generally prepared to work a summer job.'''
     
     user_content: str = f'''Summarize this resume: {resume}.'''
     
