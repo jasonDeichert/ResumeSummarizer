3abaec0644b49742faa6246369a04973c2d4c0f9
jasonDeichert <jasondeichert@gmail.com>
2024-09-08 11:33:49 -0400

    updates to db model to include ai summary

--- app/model/db/resume.py
+++ app/model/db/resume.py
@@ -3,26 +3,36 @@ from pydantic import BaseModel
 from typing import List, Optional
 
 class ResumeEducation(BaseModel):
-    degree: Optional[str]
-    major: Optional[str]
-    school: Optional[str]
-    start: Optional[str]
-    end: Optional[str]
+    degree: Optional[str] = None
+    major: Optional[str] = None
+    school: Optional[str] = None
+    start: Optional[str] = None
+    end: Optional[str] = None
 
 class ResumeExperience(BaseModel):
-    title: Optional[str]
-    company: Optional[str]
-    start: Optional[str]
-    end: Optional[str]
-    description: Optional[str]
+    title: Optional[str] = None
+    company: Optional[str] = None
+    start: Optional[str] = None
+    end: Optional[str] = None
+    description: Optional[str] = None
+
+class AISummary(BaseModel):
+    quick_summary: Optional[str] = None
+    general_employability_rating: Optional[int] = None
 
 
 class Resume(BaseModel):
-    summary: Optional[str]
-    education: Optional[List[ResumeEducation]]
-    experience: Optional[List[ResumeExperience]]
-    skills: Optional[List[str]]
-    certifications: Optional[List[str]]
-    languages: Optional[List[str]]
-    publications: Optional[List[str]]
+    summary: Optional[str] = None
+    education: Optional[List[ResumeEducation]] = None
+    experience: Optional[List[ResumeExperience]] = None
+    skills: Optional[List[str]] = None
+    certifications: Optional[List[str]] = None
+    languages: Optional[List[str]] = None
+    publications: Optional[List[str]] = None
+
+class SummarizedResume(BaseModel):
+    resume: Resume
+    ai_summary: AISummary
+
+
 
