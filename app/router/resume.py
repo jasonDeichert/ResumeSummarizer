33022e7e3a877f01259227e1dbe3434efc62bbef
jasonDeichert <jasondeichert@gmail.com>
2024-09-08 11:35:14 -0400

    more explicit typing

--- app/router/resume.py
+++ app/router/resume.py
@@ -7,12 +7,15 @@ from datetime import datetime
 
 router = APIRouter()
 
+#interestingly, fastAPI handles serialization. It was handled earlier but that was unnecessary
 @router.post("/standardizeresume", response_model=StandardizeResumeOut)
 async def standardize_resume(file: UploadFile = File(...), client: Client = Depends(Client.get_client)) -> StandardizeResumeOut:
     print(f"Received request to standardize a resume: {datetime.now()}")
-    return await resume_controller.standardize_resume(file, client)
+    standardized_resume: StandardizeResumeOut = await resume_controller.standardize_resume(file, client)
+    return standardized_resume
 
 @router.post("/summarizeresume", response_model=SummarizeResumeOut)
 async def summarize_resume(resume: StandardizeResumeOut, client: Client = Depends(Client.get_client)) -> SummarizeResumeOut:
     print(f"Received request to summarize a resume: {datetime.now()}")
-    return await resume_controller.summarize_resume(resume, client)
+    summarized_resume: SummarizeResumeOut = await resume_controller.summarize_resume(resume, client)
+    return summarized_resume
